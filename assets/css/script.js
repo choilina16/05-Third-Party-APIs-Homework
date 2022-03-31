d83194ecd1b000ae9981ec5261a304e2ef727237
Lina Choi <choilina16@gmail.com>
2022-03-30 21:45:02 -0400

    adding a javascript file

---
+++ assets/css/script.js
@@ -0,0 +1,8 @@
+// Current date to be displayed
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+
+// Current time to be displayed 
+// var time = moment().format("hh:mm:ss");
+// $("currenTime").text(time);
+
