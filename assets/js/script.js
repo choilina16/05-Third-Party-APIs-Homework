832dfb1b7598845f305a144d2205e1e6a6ea83f0
Lina Choi <choilina16@gmail.com>
2022-04-02 16:04:03 -0400

    updating script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,27 @@ $("#currentTime").text(currentTime.format('hh:mm a'));
 
 // retrieving the values from local storage to put onto the page
 // get item by the key 
-// https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
-
-
+// got help from BCS - Val !! 
+// Val said that to target multiple things at one time, you need to just put space in between. 
+$('#9 .task').val(localStorage.getItem('9'));
+$('#10 .task').val(localStorage.getItem('10'));
+$('#11 .task').val(localStorage.getItem('11'));
+$('#12 .task').val(localStorage.getItem('12'));
+$('#13 .task').val(localStorage.getItem('13'));
+$('#14 .task').val(localStorage.getItem('14'));
+$('#15 .task').val(localStorage.getItem('15'));
+$('#16 .task').val(localStorage.getItem('16'));
+$('#17 .task').val(localStorage.getItem('17'));
 
 // function to add task to local storage based on the time
 var saveButtonEl = $('.save-button');
+
 
 saveButtonEl.on('click', function() {
     // parent element for submit button and then selecting attr id
     var time = $(this).parent().attr('id');
     // sibling element for submit button and the selecting value for class task
+    // target this level. then siblings. specifically the class = task. then the value.
     var task = $(this).siblings('.task').val();
     console.log(time);
     console.log(task);
